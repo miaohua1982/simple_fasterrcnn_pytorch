384dce3a1af32cf52d93f86194b4cbef07bc4153
miaohua1982 <371462426@qq.com>
2021-04-07 17:14:07 +0800

    'add_nms_torch'

--- model/util/nms.py
+++ model/util/nms.py
@@ -1,4 +1,5 @@
 import numpy as np
+import torch as t
 from model.util.bbox_opt import calc_iou
 
 def nms(boxes, scores, iou_thresh=0.5):
@@ -6,12 +7,12 @@ def nms(boxes, scores, iou_thresh=0.5):
     non maximum supress: the boxes which have lower socre than the biggest one, and iou value bigger than iou_thresh will be supressed
     
     Args:
-        boxes(np.array): must be in shape (R, 4), where R denotes the number of boxes, and has left-top point & right-down point with(x1,y1,x2,y2)
-        scores(np.array): in shape(N,), here N==R, and every score show the confidence wether the corresponding box has objects
+        boxes(numpy.array): must be in shape (R, 4), where R denotes the number of boxes, and has left-top point & right-down point with(x1,y1,x2,y2)
+        scores(numpy.array): in shape(N,), here N==R, and every score show the confidence wether the corresponding box has objects
         iou_thresh(float):the thresh value, scores bigger than this value will be supressed
 
     Returns:
-     * **keep_idx**: the indexes which boxes should be reserved
+     * **keep_idx(numpy.array)**: the indexes which boxes should be reserved
     '''
     result = []
     arg_idx = scores.argsort()[::-1]
@@ -21,19 +22,44 @@ def nms(boxes, scores, iou_thresh=0.5):
         return arg_idx
     
     while(arg_idx.shape[0]>=2):
-        scores = scores[arg_idx]
-        boxes = boxes[arg_idx]
-        boxes_iou = calc_iou(boxes[:1], boxes[1:])
+        test_boxes = boxes[arg_idx]
+        boxes_iou = calc_iou(test_boxes[:1], test_boxes[1:])[0]   # the shape is 1*boxes.shape[0]-1
         n = boxes_iou.shape[0] # the n = boxes.shape[0]-1
-        keep_list = []
-        for i in range(n):
-            if boxes_iou[i]<=iou_thresh:
-                keep_list.append(i+1)  #0 is for the biggest score one
-        
+        keep_list = np.arange(1,n+1)[boxes_iou<=iou_thresh]
         result.append(arg_idx[0])
         arg_idx = arg_idx[keep_list]
-
+    # add last one
+    result.append(arg_idx[0])
     return np.array(result, dtype=np.int32)
-        
 
+
+def nms_torch(boxes, scores, iou_thresh=0.5):
+    '''
+
+    non maximum supress: the boxes which have lower socre than the biggest one, and iou value bigger than iou_thresh will be supressed
+    torch version
+    Args:
+        boxes(torch.tensor): must be in shape (R, 4), where R denotes the number of boxes, and has left-top point & right-down point with(x1,y1,x2,y2)
+        scores(torch.tensor): in shape(N,), here N==R, and every score show the confidence wether the corresponding box has objects
+        iou_thresh(float):the thresh value, scores bigger than this value will be supressed
+
+    Returns:
+     * **keep_idx(torch.tensor)**: the indexes which boxes should be reserved
+    '''
+    result = []
+    arg_idx = scores.argsort(descending=True)
+
+    # only one box, do nothing
+    if len(arg_idx) < 2:
+        return arg_idx
     
+    while(arg_idx.shape[0]>=2):
+        test_boxes = boxes[arg_idx]
+        boxes_iou = calc_iou(test_boxes[:1].detach().numpy(), test_boxes[1:].detach().numpy())[0]   # the shape is 1*boxes.shape[0]-1
+        n = boxes_iou.shape[0] # the n = boxes.shape[0]-1
+        keep_list = t.arange(1,n+1)[boxes_iou<=iou_thresh]
+        result.append(arg_idx[0].item())
+        arg_idx = arg_idx[keep_list]
+    # add last one
+    result.append(arg_idx[0])
+    return t.tensor(result, dtype=t.long)
