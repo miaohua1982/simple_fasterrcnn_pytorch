fcce226dd9438dd1d9311e3f8d5fd3b920658a46
miaohua1982 <371462426@qq.com>
2021-04-10 17:12:40 +0800

    add c++ version nms algorithm

--- model/util/nms_mh/src/nms_c.cpp
+++ model/util/nms_mh/src/nms_c.cpp
@@ -1,6 +1,8 @@
 
 #include<pybind11/pybind11.h>
 #include<pybind11/numpy.h>
+#include<algorithm>
+#include<vector>
 
 namespace py = pybind11;
 
@@ -16,30 +18,19 @@ inline float max(float a, float b){
     return a<b? b:a;
 }
 
-py::array_t<float> calc_iou(py::array_t<float>& boxes1, py::array_t<float>& boxes2) {
+std::vector<int> reverse_argsort(const float * array, size_t size)
+{
+	std::vector<int> array_index(size, 0);
+	for (int i = 0; i < size; ++i)
+		array_index[i] = i;
 
-    py::buffer_info boxes1_buf = boxes1.request();
-    py::buffer_info boxes2_buf = boxes2.request();
+	std::sort(array_index.begin(), array_index.end(),
+		[&array](int pos1, int pos2) {return (array[pos1] > array[pos2]);});
 
-    if ((boxes1_buf.shape[1] != 4) || (boxes2_buf.shape[1] != 4))
-    {
-        throw std::runtime_error("boxes array shape must be match [R,4]!");
-    }
+	return array_index;
+}
 
-    //access numpy.ndarray
-    float* ptr1 = (float*)boxes1_buf.ptr;
-    float* ptr2 = (float*)boxes2_buf.ptr;
-
-    size_t size1 = boxes1_buf.shape[0];
-    size_t size2 = boxes2_buf.shape[0];
-
-    //apply for new memory
-    auto result = py::array_t<float>(size1*size2);
-    //reshape to 2d array
-    result.resize({size1,size2});
-    py::buffer_info buf_result = result.request();
-    float * ptr_result = (float*)buf_result.ptr;
-
+void calc_iou_helper(const float * const ptr1, const float * const ptr2, size_t size1, size_t size2, float * ptr_result) {
     for(int i = 0; i < size1; ++i) {
         float left_top_x1 = ptr1[i*4];
         float left_top_y1 = ptr1[i*4+1];
@@ -70,13 +61,103 @@ py::array_t<float> calc_iou(py::array_t<float>& boxes1, py::array_t<float>& boxe
        }
     }
 
-    return result;
-
 }
 
-PYBIND11_MODULE(nms, m) {
+py::array_t<float> calc_iou(const py::array_t<float>& boxes1, const py::array_t<float>& boxes2) {
+
+    py::buffer_info boxes1_buf = boxes1.request();
+    py::buffer_info boxes2_buf = boxes2.request();
+
+    if ((boxes1_buf.shape[1] != 4) || (boxes2_buf.shape[1] != 4))
+    {
+        throw std::runtime_error("boxes array shape must be match [R,4]!");
+    }
+
+    //access numpy.ndarray
+    float* ptr1 = (float*)boxes1_buf.ptr;
+    float* ptr2 = (float*)boxes2_buf.ptr;
+
+    size_t size1 = boxes1_buf.shape[0];
+    size_t size2 = boxes2_buf.shape[0];
+
+    //apply for new memory
+    auto result = py::array_t<float>(size1*size2);
+    //reshape to 2d array
+    result.resize({size1,size2});
+    py::buffer_info buf_result = result.request();
+    float * ptr_result = (float*)buf_result.ptr;
+    // calc iou
+    calc_iou_helper(ptr1, ptr2, size1, size2, ptr_result);
+
+    return result;
+}
+
+
+py::array_t<int> nms(const py::array_t<float> boxes, const py::array_t<float> scores, float iou_thresh)
+{
+    py::buffer_info boxes_buf = boxes.request();
+    py::buffer_info scores_buf = scores.request();
+
+    if (boxes_buf.shape[1] != 4)
+    {
+        throw std::runtime_error("boxes array shape must be match [R,4]!");
+    }
+    
+    if (boxes_buf.shape[0] != scores_buf.shape[0])
+    {
+        throw std::runtime_error("number of boxes must be equal to number of scores!");
+    }
+
+    // access numpy.ndarray
+    float* boxes_ptr = (float*)boxes_buf.ptr;
+    float* score_ptr = (float*)scores_buf.ptr;
+    size_t size = scores_buf.shape[0];
+    // no box
+    if(size<1)
+        return py::array_t<int>();
+    // reverse argsort
+    std::vector<int> arg_idx = reverse_argsort(score_ptr, size);
+    std::vector<int> result;
+    float * test_boxes = new float[size*4]();
+    float * boxes_iou = new float[size]();
+    while(size>=2){
+        for(int i = 0; i < size; ++i)
+            memcpy(&test_boxes[i*4], &boxes_ptr[arg_idx[i]*4], 4*sizeof(float));
+
+        calc_iou_helper(test_boxes, test_boxes+4, 1, size-1, boxes_iou);   // the shape is 1*boxes.shape[0]-1
+        // keep first one
+        result.push_back(arg_idx[0]);
+        // get remainder keep list
+        size -= 1;
+        int counter = 0;
+        for(int i = 0; i < size; ++i) {
+            if(boxes_iou[i]<=iou_thresh) {
+                arg_idx[counter++] = arg_idx[i+1];  // start from 1, 0 is biggest score
+            }
+        }
+        size = counter;
+    }
+
+    delete [] test_boxes;
+    delete [] boxes_iou;
+    // add last one if need
+    if(size>0)
+        result.push_back(arg_idx[0]);
+
+    //apply for new memory for result
+    size = result.size();
+    auto keep_list = py::array_t<int>(size);
+    py::buffer_info buf_result = keep_list.request();
+    int * ptr_result = (int*)buf_result.ptr;
+    for(int i = 0; i < size; ++i)
+        ptr_result[i] = result[i];
+    return keep_list;
+}
+
+PYBIND11_MODULE(nms_mh, m) {
     m.doc() = "non-maximum supress with pybind11";
 
     m.def("add", &add, "for test");
     m.def("calc_iou", &calc_iou, "A function calculate iou between two array of boxes");
+    m.def("nms", &nms, "A function do non-maximum supress for one boxes array");
 }
--- model/util/nms_mh/tests/test.py
+++ model/util/nms_mh/tests/test.py
@@ -1,8 +1,10 @@
-import nms_mh as m
+import torch as t
 import numpy as np
+import nms_mh as m
+from torchvision.ops import nms
 
-assert m.__version__ == '0.0.1'
 assert m.add(1, 2) == 3
+
 
 def calc_iou(proposal_boxes, gt_boxes):
     lt_x = np.maximum(proposal_boxes[:,[0]], gt_boxes[:,0])
@@ -24,9 +26,81 @@ def calc_iou(proposal_boxes, gt_boxes):
     iou = inter_area/(proposal_area+gt_area-inter_area)
     # the shape of iou is num_proposal*num_gt
     return iou
+    
+def py_nms(boxes, scores, iou_thresh=0.5):
+    '''
+    non maximum supress: the boxes which have lower socre than the biggest one, and iou value bigger than iou_thresh will be supressed
+    note: well, the function's result is the same with from torchvision.ops import nms, but the speed is so not so good, 6x slower than library function
+    Args:
+        boxes(numpy.array): must be in shape (R, 4), where R denotes the number of boxes, and has left-top point & right-down point with(x1,y1,x2,y2)
+        scores(numpy.array): in shape(N,), here N==R, and every score show the confidence wether the corresponding box has objects
+        iou_thresh(float):the thresh value, scores bigger than this value will be supressed
+
+    Returns:
+     * **keep_idx(numpy.array)**: the indexes which boxes should be reserved
+    '''
+    result = []
+    arg_idx = scores.argsort()[::-1]
+    
+    while(arg_idx.shape[0]>=2):
+        test_boxes = boxes[arg_idx]
+        boxes_iou = calc_iou(test_boxes[:1], test_boxes[1:])[0]   # the shape is 1*boxes.shape[0]-1
+        n = test_boxes.shape[0]
+        keep_list = np.arange(1,n)[boxes_iou<=iou_thresh]
+        result.append(arg_idx[0])
+        arg_idx = arg_idx[keep_list]
+    # add last one
+    result.extend(arg_idx)
+    return np.array(result, dtype=np.int32)
+    
+def py_nms_cbox(boxes, scores, iou_thresh=0.5):
+    '''
+    non maximum supress: the boxes which have lower socre than the biggest one, and iou value bigger than iou_thresh will be supressed
+    note: well, the function's result is the same with from torchvision.ops import nms, but the speed is so not so good, 6x slower than library function
+    Args:
+        boxes(numpy.array): must be in shape (R, 4), where R denotes the number of boxes, and has left-top point & right-down point with(x1,y1,x2,y2)
+        scores(numpy.array): in shape(N,), here N==R, and every score show the confidence wether the corresponding box has objects
+        iou_thresh(float):the thresh value, scores bigger than this value will be supressed
+
+    Returns:
+     * **keep_idx(numpy.array)**: the indexes which boxes should be reserved
+    '''
+    result = []
+    arg_idx = scores.argsort()[::-1]
+    
+    while(arg_idx.shape[0]>=2):
+        test_boxes = boxes[arg_idx]
+        boxes_iou = m.calc_iou(test_boxes[:1], test_boxes[1:])[0]   # the shape is 1*boxes.shape[0]-1
+        n = test_boxes.shape[0]
+        keep_list = np.arange(1,n)[boxes_iou<=iou_thresh]
+        result.append(arg_idx[0])
+        arg_idx = arg_idx[keep_list]
+    # add last one
+    result.extend(arg_idx)
+    return np.array(result, dtype=np.int32)
+
+# test nms  
+rois = np.random.rand(12000,4)
+scores = np.random.rand(12000)
+
+keep1 = py_nms(rois, scores, 0.7)
+keep2 = py_nms_cbox(rois, scores, 0.7)
+keep3 = m.nms(rois, scores, 0.7)
+keep4 = nms(t.from_numpy(rois), t.from_numpy(scores), 0.7)
+
+assert np.all(keep1==keep2)
+assert np.all(keep2==keep3)
+assert np.all(keep3==keep4.numpy())
+# test speed in ipython
+#%timeit keep1 = py_nms(rois, scores, 0.7)
+#%timeit keep2 = py_nms_cbox(rois, scores, 0.7)
+#%timeit keep3 = m.nms(rois, scores, 0.7)
+#%timeit keep4 = nms(t.from_numpy(rois), t.from_numpy(scores), 0.7)
+
 
 pb = np.random.rand(12000,4)
 gb = np.random.rand(12000,4)
 iou1 = calc_iou(pb, gb)
 iou2 = m.calc_iou(pb, gb)
 assert iou1.shape == iou2.shape
+# can not do assert np.all(iou1==iou2), because nms_mh lib use float, but python code is double
